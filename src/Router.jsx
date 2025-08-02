8bd585f4373f514867e66f42eb0a6b6a1110b647
Vaishak <vaishakrajesh42@gmail.com>
2025-08-02 11:08:32 +0530

    more change

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Portfolio from './Portfolio/Portfolio';
-import Freelance from './Freelance/Freelance';
+// import Freelance from './Freelance/Freelance';
 const Router = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Portfolio />} >
                 </Route>
-                <Route path="/Freelance" element={<Freelance />} >
-                </Route>
+                {/* <Route path="/Freelance" element={<Freelance />} >
+                </Route> */}
             </Routes>
         </div>
     )
